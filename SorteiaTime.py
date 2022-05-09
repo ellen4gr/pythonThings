ca6dba6cd38312150d2abde2b9639b9ae750bc8b
Ellen <103282075+ellen4gr@users.noreply.github.com>
2022-05-09 19:53:52 -0300

    Update SorteiaTime.py

--- SorteiaTime.py
+++ SorteiaTime.py
@@ -1,26 +1,24 @@
 import random
 import PySimpleGUI
-# add nome jogadores na lista time e fatia de 5 em 5 ???
 
 listaJogadores = []
 listaTime = []
 
-# colocar N ou n
 resposta = ""
-while resposta != "N":
+while resposta != "N" and resposta!="n":
     jogadores = input("Digite o nome do jogador: ")
     listaJogadores.append(jogadores)
-    resposta = input("Deseja adicionar mais jogadores?")
-
+    resposta = input("Deseja adicionar mais jogadores? [N]ão / [S]im ?")
+#laço add jogadores na lista jogadores
 for i in range(len(listaJogadores)):
     print(listaJogadores[i])
     listaTime = listaJogadores
-
+#shuffle embaralha
 print('Essa é a lista de jogadores{}'.format(listaJogadores))
 listaTime = random.shuffle(listaJogadores)
 
 print('Essa é a lista do Time sorteado{}'.format(listaJogadores))
-
+#lista do time que recebeu os jogadores da lista jogadores
 listaTime = len(listaJogadores)
 for i in range(0, listaTime, 5):
     print('Time 5 e 5:{}'.format(listaJogadores[i:i+5]))
